43db7b180da2e009bfc740bc9c6101acfea8a3a8
lemonsaltytoo <v-lvkdl@naver.com>
2024-04-17 12:37:49 +0900

    Added cute image

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import icon from "./icon.png";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <div>
     <App />
-  </React.StrictMode>
+    <img src={icon} alt="pixel dinosaur" />
+  </div>
 );
